c0982ab2a8400515893fbf1d7a896eef0dd3fc4a
Dennis Moore <Dennis Moore>
2017-12-01 19:56:16 -0600

    committing my shizzle so I don't lizzle

--- spring/initial/src/main/java/hello/ProtoController.java
+++ spring/initial/src/main/java/hello/ProtoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import proto.MotionCommandProto;
 import proto.ProtoMessage;
 
 @Controller
@@ -15,7 +16,6 @@ public class ProtoController
     @GetMapping("/proto")
     public String greetingForm(Model model)
     {
-        System.out.println("GET");
     	model.addAttribute("protoMessage", new ProtoMessage());
         return "proto";
     }
@@ -24,13 +24,25 @@ public class ProtoController
     public String greetingSubmit(@ModelAttribute ProtoMessage protoMessage)
     {
         // This method receives the form data that was submitted to /proto.html
-    	// Using the given data, use protobuf to write to the addresses.dat file
-    	// After that, consider writing this data to a DB instead
+    	// Using the given data, construct a protobuf msg and send it over 0mq
+    	// After that, consider writing this data to a DB
     	
     	System.out.println(protoMessage.getThrottle());
     	System.out.println(protoMessage.getServo());
     	
+    	// construct motioncommandproto builder
+    	MotionCommandProto.Command.Builder motionCmdBuilder = MotionCommandProto.Command.newBuilder();
+    	motionCmdBuilder.setThrottle(protoMessage.getThrottle());
+    	motionCmdBuilder.setServo(protoMessage.getServo());
     	
+    	// protobuf messages are immutable; builders must be used to set the message fields
+    	// building the builder object returns the proto msg with the set fields
+    	MotionCommandProto.Command motionCmd = motionCmdBuilder.build();
+    	
+    	System.out.println("motionCmd.getThrottle: " + motionCmd.getThrottle());
+    	System.out.println("motionCmd.getServo: " + motionCmd.getServo());    	
+    	
+    	// now send the proto message over a zmq socket
     	
     	return "proto";
     }
